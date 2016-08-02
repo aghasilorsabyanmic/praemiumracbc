5621bcec1d1de26a0332659dd482eaab22c608dc
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-08-02 17:12:54 +0400

    Added Solution Example.

---
+++ SolutionExample/test/SolutionExample.ConsoleApplication.Text/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolutionExample.ConsoleApplication.Text
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
