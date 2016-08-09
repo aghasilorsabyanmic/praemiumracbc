9a5dfff2fb8455699d1654782b988150d35eeb58
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-08-09 16:54:52 +0400

    Lesson 03

---
+++ Training/Exceptions/MyCustomException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class MyCustomException : Exception
+    {
+        public MyCustomException()
+            : base("My Custom Exception Occured")
+        {
+            X = 5;
+        }
+
+        public MyCustomException(string message)
+            : base(message)
+        {
+            X = 5;
+        }
+
+        public MyCustomException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+            X = 5;
+        }
+
+        public MyCustomException(int y) : this()
+        {
+            Y = y;
+        }
+
+        public MyCustomException(int y, string message)
+            : this(message)
+        {
+            Y = y;
+        }
+
+        public MyCustomException(int y, string message, Exception innerException)
+            : this(message, innerException)
+        {
+            Y = y;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+    }
+}
---
+++ Training/Exceptions/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                finally
+                {
+                    Console.WriteLine("Finally");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Catch");
+            }
+            
+            
+            //TryCatchFinallyExample();
+
+        }
+
+        private static int Length(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return value.Length;
+        }
+
+        private static int? LengthAlt(string value)
+        {
+            return value?.Length;
+        }
+
+        private static void TryCatchFinallyExample()
+        {
+            try
+            {
+                var x = 1 / int.Parse(Console.ReadLine());
+                DoWork();
+            }
+            catch (IOException ex)
+            {
+
+            }
+            catch (OutOfMemoryException ex)
+            {
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+
+            }
+            catch (ArgumentNullException ex)
+            {
+
+            }
+            catch (FormatException ex)
+            {
+                throw;
+            }
+            catch (OverflowException ex)
+            {
+
+            }
+            catch (DivideByZeroException ex)
+            {
+
+            }
+            catch (StackOverflowException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        private static NotImplementedException DoWork()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+
+                throw;
+            }
+            
+            //return new NotImplementedException();
+        }
+    }
+}
