bb72fdb52a75d0315c1213ac046cd78c34a24ed1
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-09-03 11:34:16 +0400

    Failed Project :(

---
+++ Exchange/src/Exchange.Client.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exchange.Data;
+
+namespace Exchange.Client.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWork();
+        }
+
+        static async void DoWork()
+        {
+            var uri = new Uri("http://cb.am/latest.json.php");
+            var ex = new ExchangeManager(uri);
+
+            var result = await ex.Get();
+
+            System.Console.WriteLine(result.Keys.Count);
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Exchange/src/Exchange.Data/Exchange.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Data
+{
+    public sealed class ExchangeManager : IDisposable
+    {
+        public ExchangeManager(Uri url)
+        {
+            client.BaseAddress = url;
+        }
+
+        private HttpClient client = new HttpClient();
+
+        public async Task<Dictionary<string, decimal>> Get(string currency = "")
+        {
+            var url = string.IsNullOrWhiteSpace(currency) ? string.Empty : $"?currency={currency}";
+
+            var jsonString = await client.GetStringAsync(url);
+
+            var dictionary = await Task.Run(() => JsonConvert.DeserializeObject<Dictionary<string, decimal>>(jsonString));
+
+            return dictionary;
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
